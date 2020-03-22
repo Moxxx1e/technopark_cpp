bfc7946438fd605f9e8282e0a90c49d715dee020
Oleg <moxxxie07@gmail.com>
2020-03-22 15:43:06 +0300

    add src and test files for static lib

---
+++ pt_2_static/include/count.h
@@ -0,0 +1,11 @@
+#ifndef PT_2_COUNT_H
+#define PT_2_COUNT_H
+
+#include "header.h"
+
+#define NULL_ARRAY_ERROR -4
+#define INVALID_SIZE_ERROR -5
+
+int count_novoices_comments(Comment* array, int size);
+
+#endif //PT_2_COUNT_H
