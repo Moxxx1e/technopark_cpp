99eaef52f20138d3ac99c1b5acdb1dbbb59e25ec
Oleg <moxxxie07@gmail.com>
2020-03-23 13:36:06 +0300

    delete excess file

--- pt_2/run_stress.sh
+++ pt_2/run_stress.sh
@@ -4,7 +4,7 @@ list_of_files=list_of_stress_files.txt
 
 IFS=$'\n'
 
-for file in $(cat $list_of_files)
+for file in stress_test_files/*
 do
 echo "shared $file"; ./pt_2_sh $file;
 echo "static $file"; ./pt_2_st $file
