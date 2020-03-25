d2306eea103449eb41705746ae8325dff9294416
Oleg <moxxxie07@gmail.com>
2020-03-25 16:01:14 +0300

    delete errors removed to error handler header

--- pt_2/pt_2_shared/include/count.h
+++ pt_2/pt_2_shared/include/count.h
@@ -4,10 +4,6 @@
 #include "header.h"
 #include "error_handler.h"
 
-#define SYSCONF_ERROR -3
-#define PTHREAD_CREATE_ERROR -4
-#define ALLOC_FOR_THREAD_ERROR -5
-
 int count_novoices_comments(Comment* array, int size);
 
 #endif
